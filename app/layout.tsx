ece542a2ba09294f1838741440c3bd9dcddb193e
Manisha <manisha857522@gmail.com>
2025-06-30 23:35:14 +0530

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ const bricolage = Bricolage_Grotesque({
 });
 
 export const metadata: Metadata = {
-  title: "Converso",
-  description: "Real-time AI Teaching Platform",
+  title: "Ask Tia",
+  description: "Real-time AI Personal Teaching Platform",
 };
 
 export default function RootLayout({
